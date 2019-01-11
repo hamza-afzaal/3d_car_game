14932547c1c87aae0f0de13c4f8a2b668c6c81bb
Hamza <hamza.afzaal@rndtek.co.uk>
2019-01-11 23:17:58 +0500

    Initial commit to something awesome :ok_hand: :tada:

---
+++ Assets/Scenes/PlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+
+    //void fixedUpdate()
+    //{
+        
+    //}
+}
